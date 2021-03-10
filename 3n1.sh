824fe6239c2731d3b202ad6725b62354be2c7b24
Earthing007 <80304755+Earthing007@users.noreply.github.com>
2021-03-10 22:35:53 +0800

    Create 3n1.sh
    Compiled Installer for Vmess, Vless and Shdowsocks-v2ray plugin

---
+++ 3n1.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+# Compiled Installer for Vmess, Vless and Shdowsocks-v2ray plugin
+
+# Get Credentials
+get_creds (){
+	echo "\n"
+	read -p "$(echo -e '\033[1;36mCertificate: \033[1;34m')" CERT
+	read -p "$(echo -e '\033[1;36mKey: \033[1;34m')" KEY
+	read -p "$(echo -e '\033[1;36mDomain: \033[1;34m')" DOM
+    echo -e "\033[0m"
+    str=`echo $DOM | grep '^\([a-zA-Z0-9_\-]\{1,\}\.\)\{1,\}[a-zA-Z]\{2,5\}'`
+    while [ ! -n "${str}" ]
+    do
+        echo "\033[1;31mInvalid domain, please try again\033[0m"
+		read -p "$(echo -e '\033[1;36mDomain: \033[1;34m')" DOM
+        str=`echo $DOM | grep '^\([a-zA-Z0-9_\-]\{1,\}\.\)\{1,\}[a-zA-Z]\{2,5\}'`
+    done
+}
+
+# Install v2ray and nginx-full
+install_v2ray (){
+	[[ ! -f /usr/local/bin/v2ctl ]] && bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+	[[ ! -f /etc/nginx/nginx.conf ]] && apt install nginx-full -y
+}
+
+# Get Cert & Key
+cert_key (){
+	if [[ ! -f /usr/local/etc/v2ray/fullchain.pem ]]; then
+		mkdir /usr/local/etc/v2ray/
+		wget --no-check-certificate -O /usr/local/etc/v2ray/cert.pem "$CERT"
+		wget --no-check-certificate -O /usr/local/etc/v2ray/key.pem "$KEY"
+		curl -kL "https://support.cloudflare.com/hc/article_attachments/360037898732/origin_ca_ecc_root.pem" -o /usr/local/etc/v2ray/root_ecc.pem
+		v2raydir='/usr/local/etc/v2ray' && printf "%b\n" "$(cat $v2raydir/cert.pem)\n$(cat $v2raydir/cert.pem)\n$(cat $v2raydir/root_ecc.pem)" > $v2raydir/fullchain.pem
+	else
+		:
+	fi
+}
+
+# Configure Vmess
+vmess_conf () {
+	UUID=$(v2ctl uuid)
+	v2rayconf='/usr/local/etc/v2ray/vmess.json' && nginxv2conf='/etc/nginx/conf.d/v2ray.conf' && gistlink='https://gist.github.com/Earthing007/a540470d6042680fbf88fdaa1a8e0e78/raw' && curl -kL "$gistlink/vmess.json" -o $v2rayconf && curl -kL "$gistlink/v2ray.conf" -o $nginxv2conf && sed -i "s|SERVER_DOMAIN|$DOM|g;s|GENERATED_UUID_CODE|$UUID|g" $v2rayconf && sed -i "s|DOMAIN_HERE|$DOM|g" $nginxv2conf
+	rm -rf /etc/nginx/{default.d,conf.d/default.conf,sites-*}
+}
+
+# Configure Vless
+vless_conf (){
+	UUID=$(v2ctl uuid)
+	v2rayconf='/usr/local/etc/v2ray/vless.json' && nginxv2conf='/etc/nginx/conf.d/v2ray.conf' && gistlink='https://gist.github.com/Earthing007/a540470d6042680fbf88fdaa1a8e0e78/raw' && curl -kL "$gistlink/vless.json" -o $v2rayconf && curl -kL "$gistlink/v2ray.conf" -o $nginxv2conf && sed -i "s|SERVER_DOMAIN|$DOM|g;s|GENERATED_UUID_CODE|$UUID|g" $v2rayconf && sed -i "s|DOMAIN_HERE|$DOM|g" $nginxv2conf
+	rm -rf /etc/nginx/{default.d,conf.d/default.conf,sites-*}
+}
+
+# Kill ports
+kill_ports (){
+	if [[ $(netstat -tlnp | grep -E ':80' | awk '{print $4}' | sed -e 's/.*://') = 80 ]]; then
+		echo -e "\033[1;33mKilling process running on port 80..\033[0m"
+		kill $(lsof -t -i :80)
+	fi
+		if [[ $(netstat -tlnp | grep -E ':443' | awk '{print $4}' | sed -e 's/.*://') = 443 ]]; then
+		echo -e "\033[1;33mKilling process running on port 443..\033[0m"
+		kill $(lsof -t -i :443)
+	fi
+	if [[ $(netstat -tlnp | grep -E ':10808' | awk '{print $4}' | sed -e 's/.*://') = 10808 ]]; then
+		echo -e "\033[1;33mKilling process running on port 10808..\033[0m"
+		kill $(lsof -t -i :10808)
+	fi
+}
+
+# Start services
+start_services (){
+	systemctl start v2ray 2>/dev/null && systemctl restart nginx
+	systemctl enable v2ray
+	sleep 3
+}
+
+# Print Info
+info (){
+	port=$(netstat -tlnp | grep -E ':443' | awk '{print $4}' | sed -e 's/.*://')
+	clear
+	netstat -tlnp | grep -E '(:10808|:443|:80)'
+	echo -e "\n"
+	echo -e "\033[1;32mAccount Details:\033[0m\n\n"
+	echo -e "\033[1;33mUUID:\033[1;36m $UUID\033[0m\n"
+	echo -e "\033[1;33mHost:\033[1;36m $DOM\033[0m\n"
+	echo -e "\033[1;33mPort:\033[1;36m $port\033[0m\n"
+	echo -e "\n"
+}
+
+# Install Vmess
+install_vmess (){
+	get_creds
+	install_v2ray
+	cert_key
+	vmess_conf
+	kill_ports
+	start_services
+	info
+}
+
+# Install Vless
+install_vless (){
+	get_creds
+	install_v2ray
+	cert_key
+	vless_conf
+	kill_ports
+	start_services
+	info
+}
+
+# Install SS with V2ray plugin
+install_ss (){
+	rm -f ss.sh && wget -O ss.sh 'https://raw.githubusercontent.com/Earthing007/private-scripts/main/ss-v2ray?token=ATEVU42KDH2FSNTB75LFMZDAJDME6' && chmod +x ss.sh && ./ss.sh
+}
+
+# Misc
+misc (){
+	if [[ ! -f /etc/iptables/rules.v4 ]]; then
+		rm -f misc.sh && wget -O misc.sh 'https://raw.githubusercontent.com/Earthing007/private-scripts/main/misc.sh?token=ATEVU4YHWAGOCFBIIRS6CEDAJDL76' && chmod +x misc.sh && ./misc.sh
+		rm -f misc.sh
+	else
+		:
+	fi
+}
+
+update (){
+	export DEBIAN_FRONTEND=noninteractive
+	apt update && apt upgrade -y -f
+	apt install curl wget unzip net-tools lsof -y
+}
+
+menu (){
+	# Color
+	RD='\033[0;31m' # Red
+	GR='\033[0;32m' # Green
+	CY='\033[0;36m' # Cyan
+	NC='\033[0m' # No Color
+	clear
+	echo -e "${GR}What do you want to do?${NC}\n"
+	echo -e "[1] ${CY}Install Vmess${NC}"
+	echo -e "[2] ${CY}Install Vless${NC}"
+	echo -e "[3] ${CY}Install Shadowsocks${NC}"
+	echo -e "[4] ${CY}Enable Optimizations(experimental)${NC}\n"	
+	read -p "$(echo -e 'Choose from options \e[32m[\e[0m1-4\e[32m]\e[0m: ')" option
+	case $option in
+			1)
+			update
+			install_vmess
+			;;
+			2)
+			update
+			install_vless
+			;;
+			3)
+			update
+			install_ss
+			;;
+			4)
+			misc
+			[[ $(netstat -tulpn | grep "nginx") ]] && systemctl restart v2ray && systemctl restart nginx && echo -e "${GR}done${NC}"
+			[[ $(netstat -tulpn | grep "ss-server") ]] && systemctl restart shadowsocks && echo -e "${CY}done${NC}"
+			;;			
+			*)
+			echo -e "${RD}Invalid, please try again.${NC}\n"
+			sleep 2
+			menu
+			;;
+		esac
+}
+
+# Verify root
+if [ "$(whoami)" != 'root' ]; then
+	echo -e " \e[33m This script must be run as root\e[0m"
+	exit
+else
+	menu
+fi
