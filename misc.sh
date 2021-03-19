b068a841ffd677d74c56446488eeec2a0cf5f992
Earthing007 <80304755+Earthing007@users.noreply.github.com>
2021-03-19 15:05:01 +0800

    Update misc.sh

--- misc.sh
+++ misc.sh
@@ -112,6 +112,17 @@ iptables-restore < /etc/iptables/rules.v4
 netfilter-persistent save
 systemctl enable netfilter-persistent
 
+# Install speedtest by ookla
+sudo apt-get install gnupg1 apt-transport-https dirmngr
+export INSTALL_KEY=379CE192D401AB61
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+echo "deb https://ookla.bintray.com/debian generic main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
+sudo apt-get update
+# Other non-official binaries will conflict with Speedtest CLI
+# Example how to remove using apt-get
+# sudo apt-get remove speedtest-cli
+sudo apt-get install speedtest
+
 #BBR
 kernel=$(uname -r | awk -F- '{print $1}' | tr -d '.')
 checkBBR () {
@@ -213,14 +224,3 @@ else
 	rm mycron
 	systemctl enable cron && systemctl restart cron
 fi
-
-# Install speedtest by ookla
-sudo apt-get install gnupg1 apt-transport-https dirmngr
-export INSTALL_KEY=379CE192D401AB61
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
-echo "deb https://ookla.bintray.com/debian generic main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
-sudo apt-get update
-# Other non-official binaries will conflict with Speedtest CLI
-# Example how to remove using apt-get
-# sudo apt-get remove speedtest-cli
-sudo apt-get install speedtest
