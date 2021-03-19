05d1e6a3b39e396927adb43c4daaa3911b8508e3
Earthing007 <80304755+Earthing007@users.noreply.github.com>
2021-03-19 15:48:44 +0800

    Update misc.sh

--- misc.sh
+++ misc.sh
@@ -113,7 +113,7 @@ netfilter-persistent save
 systemctl enable netfilter-persistent
 
 # Install speedtest by ookla
-apt-get install gnupg1 apt-transport-https dirmngr
+apt-get install -y gnupg1 apt-transport-https dirmngr
 export INSTALL_KEY=379CE192D401AB61
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
 echo "deb https://ookla.bintray.com/debian generic main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
